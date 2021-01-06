79b189919810062120acba3b5f8e9bdc26ff5205
RaimondsMezalsQA <RMezals@qa.com>
2021-01-06 16:40:15 +0000

    Added 99_bottles-of-beer exercise

---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,24 @@
+package com.qa.main;
+
+public class Runner {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 99; i >= 1;) {
+			  System.out.println(i + " bottles of beer on the wall, 99 bottles of beer. ");
+			  i-=1;
+			  System.out.println("Take one down and pass it around, " + i +" bottles of beer on the wall.");
+			  
+			}
+		System.out.println("No more bottles of beer on the wall, no more bottles of beer.\r\n" + 
+				"Go to the store and buy some more, 99 bottles of beer on the wall.");
+	
+
+
+		
+	}
+
+}
